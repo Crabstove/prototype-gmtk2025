e7052178d631b5a5a42a42482a2aaa275c775770
Florian <45694132+flo-bit@users.noreply.github.com>
2025-08-03 15:11:46 +0200

    start level design

--- src/data/level.data.ts
+++ src/data/level.data.ts
@@ -9,10 +9,12 @@ export interface PlatformData {
 }
 
 export const LEVEL_1_PLATFORMS: PlatformData[] = [
-  { x: 0, y: 550, width: 1600, height: 50, color: PLATFORM_COLORS.GROUND },
-  { x: 200, y: 450, width: 150, height: 20, color: PLATFORM_COLORS.PLATFORM },
-  { x: 450, y: 350, width: 120, height: 20, color: PLATFORM_COLORS.PLATFORM },
-  { x: 650, y: 400, width: 100, height: 20, color: PLATFORM_COLORS.PLATFORM },
-  { x: 850, y: 300, width: 130, height: 20, color: PLATFORM_COLORS.PLATFORM },
-  { x: 1100, y: 250, width: 110, height: 20, color: PLATFORM_COLORS.PLATFORM },
+  { x: 0, y: 550, width: 800, height: 50, color: PLATFORM_COLORS.GROUND },
+  { x: 900, y: 550, width: 300, height: 50, color: PLATFORM_COLORS.PLATFORM },
+
+
+  { x: 1300, y: 350, width: 150, height: 50, color: PLATFORM_COLORS.PLATFORM },
+
+  { x: 1450, y: 450, width: 800, height: 50, color: PLATFORM_COLORS.PLATFORM },
+
 ];
