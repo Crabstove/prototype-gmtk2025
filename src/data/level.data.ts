3fa6c4e86933797775993ed7c645dcd157dbf3e3
Florian <45694132+flo-bit@users.noreply.github.com>
2025-08-03 18:25:46 +0200

    add random platforms

--- src/data/level.data.ts
+++ src/data/level.data.ts
@@ -17,4 +17,22 @@ export const LEVEL_1_PLATFORMS: PlatformData[] = [
 
   { x: 1450, y: 450, width: 800, height: 50, color: PLATFORM_COLORS.PLATFORM },
 
+  { x: 200, y: 450, width: 150, height: 20, color: PLATFORM_COLORS.PLATFORM },
+  { x: 450, y: 350, width: 120, height: 20, color: PLATFORM_COLORS.PLATFORM },
+  { x: 650, y: 400, width: 100, height: 20, color: PLATFORM_COLORS.PLATFORM },
+  { x: 850, y: 300, width: 130, height: 20, color: PLATFORM_COLORS.PLATFORM },
+  { x: 1100, y: 250, width: 110, height: 20, color: PLATFORM_COLORS.PLATFORM },
+
+
+  { x: 0, y: 250, width: 150, height: 20, color: PLATFORM_COLORS.PLATFORM },
+  { x: 850, y: 100, width: 120, height: 20, color: PLATFORM_COLORS.PLATFORM },
+  { x: 600, y: 300, width: 100, height: 20, color: PLATFORM_COLORS.PLATFORM },
+  { x: 500, y: 200, width: 130, height: 20, color: PLATFORM_COLORS.PLATFORM },
+  { x: 200, y: 150, width: 110, height: 20, color: PLATFORM_COLORS.PLATFORM },
+
+  { x: 200, y: 0, width: 150, height: 20, color: PLATFORM_COLORS.PLATFORM },
+  { x: 450, y: -100, width: 120, height: 20, color: PLATFORM_COLORS.PLATFORM },
+  { x: 650, y: -50, width: 100, height: 20, color: PLATFORM_COLORS.PLATFORM },
+  { x: 850, y: -150, width: 130, height: 20, color: PLATFORM_COLORS.PLATFORM },
+  { x: 1100, y: -200, width: 110, height: 20, color: PLATFORM_COLORS.PLATFORM },
 ];
